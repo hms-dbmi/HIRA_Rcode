84b134a801f7afbcf3bde800271f9c3705c5f7e1
Arnaud Serret-Larmande <arnaudsl12@gmail.com>
2020-05-14 14:55:32 +0200

    tidying and removing example code

--- deathPheWAS/HIRA_codeDeathPheWAS.R
+++ deathPheWAS/HIRA_codeDeathPheWAS.R
@@ -12,7 +12,10 @@ library(readxl)
 library(tidyverse)
 library(lubridate)
 
-####### UTILS
+##########################
+# Utils #
+##########################
+
 factory <- function(fun){
   function(...) {
     warn <- err <- NULL
@@ -41,6 +44,9 @@ factory_confint <- function(...) {
   return(out$value)
 }
 
+##########################
+# DATA MANAGEMENT #
+##########################
 #corona claim data
 co19_t200_trans_dn = read_excel(covid_file_path,
                                 sheet=2)
@@ -53,30 +59,6 @@ co19_t200_twjhe_dn = read_excel(covid_file_path, sheet=6)
 #medication for medical use history data
 co19_t530_twjhe_dn = read_excel(covid_file_path, sheet=9)
 
-# ## ONLY FOR SAMPLE DATA: to have severe ICD codes in sample data
-co19_t200_trans_dn[co19_t200_trans_dn$MAIN_SICK == "J029", "MAIN_SICK"] <- "J80"
-co19_t200_trans_dn[co19_t200_trans_dn$SUB_SICK == "J029", "SUB_SICK"] <- "J80"
-co19_t530_twjhe_dn[is.na(co19_t200_twjhe_dn$MAIN_SICK), "MAIN_SICK"] <- "J80"
-co19_t530_twjhe_dn[is.na(co19_t200_twjhe_dn$SUB_SICK), "SUB_SICK"] <- "J80"
-co19_t530_trans_dn[co19_t530_trans_dn$GNL_CD %in% c("430101ATB", "179303ATE"), "GNL_CD"] <- "450200BIJ"
-co19_t530_twjhe_dn[co19_t530_twjhe_dn$GNL_CD %in% c("248902ATB", "179303ATE"), "GNL_CD"] <- "450200BIJ"
-
-# ONLY FOR SAMPLE DATA: data management for sample medical history, to have overlapping MID
-co19_t200_twjhe_dn$MID <- sample(co19_t200_trans_dn$MID,
-                                 length(co19_t200_twjhe_dn$MID),
-                                 replace=T)
-co19_t530_twjhe_dn$MID <- sample(co19_t200_trans_dn$MID,
-                                 length(co19_t530_twjhe_dn$MID),
-                                 replace=T)
-co19_t530_trans_dn$MID <- sample(co19_t200_trans_dn$MID,
-                                 length(co19_t530_trans_dn$MID),
-                                 replace=T)
-co19_t200_twjhe_dn[co19_t200_twjhe_dn$RECU_FR_DD %in% c(20190101, 20150404),'RECU_FR_DD'] <- "20200101"
-#ONLY FOR SAMPLE DATA: hospitalization patients
-co19_t200_trans_dn[1, "FOM_TP_CD"] <- "021"
-co19_t200_twjhe_dn[1, "FOM_TP_CD"] <- "021"
-# Adding death
-co19_t200_trans_dn[c(4,6), "DGRSLT_TP_CD"] <- "4"
 
 #SEX_TP_CD to sex
 sex_recoding <- c("male" = "1",
@@ -163,6 +145,9 @@ wide_t200 <- spread(long_t200[c("MID", "RECU_FR_DD", "RECU_TO_DD", "before_since
                     value = "present")
 phewas_df <- left_join(person_table, wide_t200, by = "MID")
 
+##########################
+# STATS FUNCTIONS #
+##########################
 
 response_var <- "death"
 glm_process <- function(phewas_df,
@@ -246,7 +231,7 @@ descriptive_stats <- function(phewas_df, person_table, long_t200, unique_icd_3d)
 factory_descriptive_stats <- factory(descriptive_stats)
 
 
-########## COMPUTATIONS
+########## COMPUTATIONS --> OUTPUTS
 ##############
 output_phewas_whole <- factory_phewas_function(phewas_df, unique_icd_3d)
 
@@ -265,7 +250,9 @@ output_descriptive_stats <- factory_descriptive_stats(phewas_df,
                                                       long_t200,
                                                       unique_icd_3d)
 
-##  DATA TO EXPORT: DESCRIPTIVE STATISTICS AND PHEWAS PROCESS RESULTS
+##########################
+# DATA TO EXPORT #
+##########################
 save(output_phewas_whole,
      output_phewas_evere_severe,
      output_phewas_before_since,
